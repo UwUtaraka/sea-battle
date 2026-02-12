5501d3105e068f9e6cac61e55aa2b5c74a3d9e59
zyuzya <vadim.korneev2007@gmail.com>
2026-02-12 11:54:31 -0800

    new method for adding ships

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@ using namespace std;
 class Game{
     public:
     char field[SIZE][SIZE]{0};
+    int boat = 2;
+    int destroyer = 1;
+
     Game(){
         for (int i = 0; i < SIZE; i++){
             for (int j = 0; j < SIZE; j++){
-                field[i][j] = '=';
+                field[i][j] = '-';
             }
         }
     }
@@ -23,25 +26,60 @@ class Game{
             cout << endl;
         }
     }
-    void makeMove(){
-        cout << "введите координаты: ";
-        int x, y;
-        cin >> x >> y;
-        field[x][y] = '#';
+
+    void placement(){
+        for (int i = 0; i < destroyer; i++){
+            int x, y;
+            string orientation;
+            cout << "введите координаты для эсминца и его положение(вертикальное/горизонтальное(V/H)): ";
+            cin >> x >> y >> orientation;
+            if (orientation == "V"){
+                if (x >= 0 && x + 1 < SIZE && y >= 0 && y < SIZE){
+                    field[x][y] = '#';
+                    field[x + 1][y] = '#';
+                }
+                else{cout << "корабль выходит за границу" << endl;}
+            }    
+            else if (orientation == "H"){
+                if (x >= 0 && x < SIZE && y >= 0 && y + 1 < SIZE){
+                    field[x][y] = '#';
+                    field[x][y + 1] = '#';
+                }
+                else{cout << "корабль выходит за границу" << endl;}
+            }
+        }
+        for (int i = 0; i < boat; i++){
+            int x, y;
+            cout << "введите координаты для катера: ";
+            cin >> x >> y;
+            if (field[x][y] != '#'){
+                field[x][y] = '#';
+            }
+            else{cout << "данная клетка занята" << endl;}
+            
+        }
     }
-    
 };
 
 int main(){
     Game game1;
     Game game2;
+    cout << "Player 1" << endl;
     game1.show();
-    game1.makeMove();
+    game1.placement();
     game1.show();
-    cout << endl;
+    cout << "для передачи управления нажмит Enter" << endl;
+
+    cin.get();
+    cin.get();
+
+    system("cls");
 
     cout << endl;
 
-    game2.makeMove();
-    cout << endl;
+    cout << "Player 2" << endl;
+    game2.show();
+    game2.placement();
+    game2.show();
+
 }
