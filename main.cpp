373244e5830f251830ed918beb6c4d5cee35ee33
zyuzya <vadim.korneev2007@gmail.com>
2026-02-12 08:49:26 -0800

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Game{
+    public:
+    int field[10][10]{};
+};
+int main(){
+    Game game;
+    for (size_t i = 0; i < sizeof(game.field) / sizeof(game.field[0]); i++){
+        for (size_t j = 0; j < sizeof(game.field) / sizeof(game.field[0]); j++){
+            cout << game.field[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
