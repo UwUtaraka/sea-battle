3324d78f5e8f6ca7e8849fba5da0f792f88acfa2
zyuzya <vadim.korneev2007@gmail.com>
2026-02-12 09:55:47 -0800

    add points

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <string>
+#define SIZE 5
 
 using namespace std;
 
 class Game{
     public:
-    int field[5][5]{};
-};
-int main(){
-    Game game;
-    for (size_t i = 0; i < sizeof(game.field) / sizeof(game.field[0]); i++){
-        for (size_t j = 0; j < sizeof(game.field) / sizeof(game.field[0]); j++){
-            cout << game.field[i][j] << " ";
+    char field[SIZE][SIZE]{0};
+    void create(){
+        for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++){
+                field[i][j] = '=';
+            }
         }
-        cout << endl;
     }
+    void makeMove(){
+        cout << "введите координаты: ";
+        int x, y;
+        cin >> x >> y;
+        field[x][y] = '#';
+    }
+    void show(){
+        for (size_t i = 0; i < SIZE; i++){
+            for (size_t j = 0; j < SIZE; j++){
+                cout << field[i][j] << " ";
+            }
+            cout << endl;
+        }
+    };
+};
+
+int main(){
+    Game game1;
+    Game game2;
+    game1.create();
+    game1.show();
+    game1.makeMove();
+    cout << endl;
+    game1.show();
+
+    cout << endl;
+
+    game2.show();
+    game2.makeMove();
+    cout << endl;
+    game2.show();
 }
